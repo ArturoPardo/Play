d87580474532c80bdd558395dc192f64c3712c25
Arturo Pardo <arturopardosanchez@gmail.com>
2019-02-24 15:40:55 +0100

    up-right-part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,37 @@ export class AppComponent {
   
     this.triangles.push(text);
     console.log(this.triangles);
+    if(this.triangles.length >1){
+      
+  
+        // ----triangulo----------
+var list = document .getElementsByClassName("cont1");
+var pes =-90;
+var idea = setInterval(rotate, 0.1);
+function rotate() {
+  if (pes ==0) {
+    clearInterval(idea);
+    console.log("girando");
+  }
+  else{
+    pes++; 
+    console.log(pes);
+    list[0].style.transform ='rotate(' +pes+'deg)'; 
+   
+  }
+
+}
+// ----triangulo----------
+    
+    
+    }
+  
     var lista = document.getElementById("lista"); 
     lista.style.display = "none"; 
+  
     var circle = document.getElementById("circle"); 
-        circle.style.display = "none"; 
+    circle.style.display = "none"; 
+
 
 
 
@@ -49,11 +76,12 @@ function rotate() {
         console.log("50");
         clearInterval(id);
 // ----circle-----------
-        var circle = document.getElementById("circle"); 
-        circle.style.display = "block"; 
+       
         var pas= 0; 
         var ide = setInterval(uper, 15);
         function uper() {
+          var circle = document.getElementById("circle"); 
+          circle.style.display = "block"; 
           if (pas ==90) {
             clearInterval(ide);
 // ----lista-----------
