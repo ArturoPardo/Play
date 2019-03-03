d55957305afb3893010c482796ca384cc5be1673
Arturo Pardo <arturopardosanchez@gmail.com>
2019-03-03 13:36:26 +0100

    creating service & list & board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreacionComponent } from './creacion/creacion.component';
 import { ListadoComponent } from './listado/listado.component';
+import { ListComponent } from './list/list.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreacionComponent,
-    ListadoComponent
+    ListadoComponent,
+    ListComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data-manager.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { List } from './models.interface';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataManagerService {
+  data: { lists: Array<List> } = {
+    lists: [
+      {
+        listId: 0,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        name: 'to do',
+        tasks: [
+          {
+            listId: 0,
+            taskId: 0,
+            text: 'aprender angular',
+            completed: false,
+            color: 'white',
+            createdAt: new Date(),
+            modifiedAt: new Date(),
+          },
+          {
+            listId: 0,
+            taskId: 1,
+            text: 'aprender js',
+            completed: false,
+            color: 'white',
+            createdAt: new Date(),
+            modifiedAt: new Date(),
+          },
+        ],
+      },
+      {
+        listId: 1,
+        createdAt: new Date(),
+        modifiedAt: new Date(),
+        name: 'doing',
+        tasks: [
+          {
+            listId: 1,
+            taskId: 0,
+            text: 'aprender typescript',
+            completed: false,
+            color: 'white',
+            createdAt: new Date(),
+            modifiedAt: new Date(),
+          },
+        ],
+      },
+    ],
+  };
+  // --------------------------- fin---------------
+
+  getData() {
+    return this.data;
+  }
+  addNewList(name: string) {
+    const now = new Date();
+    const newList: List = {
+      listId: Date.now(),
+      createdAt: now,
+      modifiedAt: now,
+      name,
+      tasks: [],
+    };
+    this.data.lists.push(newList);
+  }
+  deleteList(listId: number) {
+    this.data.lists = this.data.lists.filter(list => list.listId !== listId);
+  }
+}
+
